7c777f0e68aa88817a3f4eebb16da163d1058a38
Juan Delgado <roloenusa@msn.com>
2024-06-06 14:11:39 -0700

    Update bufferline to handle buffers in a more useful way

--- lua/roloenusa/plugins/bufferline.lua
+++ lua/roloenusa/plugins/bufferline.lua
@@ -1,11 +1,26 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-    },
-  },
+  'akinsho/bufferline.nvim',
+  requires = 'nvim-tree/nvim-web-devicons', -- Optional, for file icons
+  config = function()
+    require('bufferline').setup {
+      options = {
+        numbers = "both", -- Shows buffer index and number
+        diagnostics = "nvim_lsp", -- Show LSP diagnostics in the bufferline
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        separator_style = "slant", -- Or "thick", "thin", etc.
+        always_show_bufferline = true,
+      },
+    }
+
+    -- Keybindings to navigate between buffers
+    -- Ommitting this for now so i can better learn the buffer navigation:
+    -- :ls -- List all buffers
+    -- :bp -- previous
+    -- :bn -- next
+    -- :b# -- jump to buffer
+    -- vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+    -- vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+  end
 }
 
