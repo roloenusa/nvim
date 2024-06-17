f38deaa8f73c61916952f0fd841b60f130998024
roloenusa <roloenusa@msn.com>
2024-06-16 23:11:55 -0700

    Updating theme and lock

--- lua/roloenusa/plugins/colorscheme.lua
+++ lua/roloenusa/plugins/colorscheme.lua
@@ -2,18 +2,17 @@ return {
     {
         "bluz71/vim-nightfly-colors",
         name = "nightfly",
-        lazy = false,
         priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme nightfly]])
-        end
+        -- config = function()
+        --     vim.cmd([[colorscheme nightfly]])
+        -- end
     }, {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme catppuccin]])
-        end
+        -- config = function()
+        --     vim.cmd([[colorscheme catppuccin]])
+        -- end
 
     }
 }
